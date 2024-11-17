17b0ad2cb55230fe39a46bc43e15bf4a01f8e848
Sameeu20 <abdulsameeu9@gmail.com>
2024-11-17 17:36:01 +0530

    Fixed Product Confirmation Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 export const routes: Routes = [
+
+
+    {path:'',component:ProductDetailsComponent},
     { path: 'order-confirmation', component: OrderConfirmationComponent },
 ];
